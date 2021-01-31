aa4af1429fdcd64d46e0a1a533ffa2557773daa9
Miika Vähänen <miika@Miika-MBP.home>
2021-01-31 21:19:34 +0200

    First commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,73 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	private String title;
+	private String author;
+	private String year;
+	private String isbn;
+	private String price;
+	
+	public Book() {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public Book(String title, String author, String year, String isbn, String price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+}
---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,19 @@
+package com.example.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookController {
+	@RequestMapping(value= "/index", method = RequestMethod.GET)
+	public String book() {
+		return "Moro";
+	}
+}
